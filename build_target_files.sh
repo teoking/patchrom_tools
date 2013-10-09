bd0cc2e701131cb88d94f3badf84f1c18f8e4446
teoking <yagatong@gmail.com>
2013-10-09 18:39:35 +0800

    1. add -n parameter to format the /data in the update-script 2. memory adjustment when signing apks

--- build_target_files.sh
+++ build_target_files.sh
@@ -88,7 +88,7 @@ function sign_target_files {
 # build a new full ota package
 function build_ota_package {
     echo "Build full ota package: $OUT_DIR/$OUT_ZIP_FILE"
-    $OTA_FROM_TARGET_FILES -n -k $PORT_ROOT/build/security/testkey $TARGET_FILES_ZIP $OUT_DIR/$OUT_ZIP_FILE
+    $OTA_FROM_TARGET_FILES -n -w -k $PORT_ROOT/build/security/testkey $TARGET_FILES_ZIP $OUT_DIR/$OUT_ZIP_FILE
 }
 
 
--- releasetools/common.py
+++ releasetools/common.py
@@ -356,7 +356,7 @@ def SignFile(input_name, output_name, key, password, align=None,
   else:
     sign_name = output_name
 
-  cmd = ["java", "-Xmx1024m", "-jar",
+  cmd = ["java", "-Xmx2048m", "-jar",
            os.path.join(OPTIONS.search_path, "signapk.jar")]
   if whole_file:
     cmd.append("-w")
